0e1b6f1e31646fe1c6afedb9e71f1c20a163f42a
danieeel984 <danielcanrinus@gmail.com>
2025-03-09 11:27:30 +0100

    Add files via upload

---
+++ os in c.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	printf("this is an os! \n");
+    int myNum;
+
+// Ask the user to type a number
+printf("Type a number: \n");
+
+// Get and save the number the user types
+scanf("%d \n", &myNum);
+printf("you typed: %d", myNum);
+
+}
+
